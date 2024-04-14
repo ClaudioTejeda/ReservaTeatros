6140b6fd95859dc1fdaf763c45bf62e6868a8d5d
ClaudioTejeda <claudiotejeda945@gmail.com>
2024-04-14 10:31:31 -0600

    Creacion proyecto teatro

---
+++ CapaDatos/BasedeDatos/ExamenContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.BasedeDatos
+{
+    public class ExamenContext : DbContext
+    {
+
+        public ExamenContext() : base("Teatro")
+        {
+
+        }
+    }
+}
